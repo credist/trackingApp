d0f65ddef80782cade10584059f9ca27bb9c9a8b
credist <n.ehrenfeld2@gmail.com>
2025-02-13 20:59:57 +0100

    create

---
+++ trackingApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace trackingApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
